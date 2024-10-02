494f125bf41b31796d146d027da910b41ba25a78
27areeser <reeser_a@ix.bhasd.local>
2024-10-02 08:55:24 -0400

    Change made to pets

--- Main.java
+++ Main.java
@@ -16,13 +16,46 @@ class Main {
   public static void main(String[] args) {
    // zero argument pet constructor
    Pet pet1 = new Pet();
+   System.out.println("First dog:");
+   System.out.println(pet1.toString());
+   System.out.println();
 
-   // Three 6 argment pet constructors
-   Pet pet2 = new Pet("Dog", "Wool", 10, 48.5, false, true);
+   // First 6 argment pet constructor
+   Pet pet2 = new Pet("Husky", "Apollo", 8, 59.5, false, true);
+   System.out.println("Second dog:");
+   System.out.println(pet2.toString());
+   System.out.println();
 
-   System.out.println(pet1);
-   System.out.print(pet2);
+   //Second 6 argument pet constructor
+   Pet pet3 = new Pet("Maltese", "Penny", 5, 6.8, false, false);
+   System.out.println("Third dog:");
+   System.out.println(pet3.toString());
+   System.out.println();
+
+   //Third 6 argument pet constructor
+   Pet pet4 = new Pet("Goldendoodle", "Max", 9, 55.6, true, true);
+   System.out.println("Fourth Dog:");
+   System.out.println(pet4.toString());
+   System.out.println();
+  
    
+
+   // Excersing sleep method on pet1 object
+   System.out.println("Pet actions:");
+   pet1.sleep();
+
+   // Excersizing wakeUp method on pet2 object
+   pet2.wakeUp();
+
+   // Excersizing aging method on pet1 and pet3
+   pet1.aging(4);
+   pet2.aging(5);
+
+   // Excersizing changeWeight method on pet4 and pet2
+   // Gain weight:
+   pet4.changeWeight(5.0);
+  // Lose weight
+   pet2.changeWeight(-4.6);
 
 
 
